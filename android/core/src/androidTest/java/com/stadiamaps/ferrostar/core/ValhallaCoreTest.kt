a1444d90c967a1f9e93a9132908095058d3a080a
Ian Wagner <ian.wagner@stadiamaps.com>
2023-11-06 01:27:35 +0900

    Try adding a manual action to run android emulator tests

--- android/core/src/androidTest/java/com/stadiamaps/ferrostar/core/ValhallaCoreTest.kt
+++ android/core/src/androidTest/java/com/stadiamaps/ferrostar/core/ValhallaCoreTest.kt
@@ -2,6 +2,7 @@
  * FIXME: This file should move out of Android Tests ASAP.
  * It only exists here because I haven't yet figured out how to build and link the platform-native
  * binaries via JNI just yet and this works.
+ * See https://github.com/willir/cargo-ndk-android-gradle/issues/12.
  *
  * This solution is STUPIDLY INEFFICIENT and will probably contribute to global climate change since
  * an Android emulator uses like two whole CPU cores when idling.
