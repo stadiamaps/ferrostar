29ebf2943e85a28e049499ade343c456c6c9572c
Jacob Fielding <jf0517@gmail.com>
2024-03-04 21:25:00 -0800

    fix: corrected multiple task bug

--- apple/Sources/FerrostarCore/FerrostarCore.swift
+++ apple/Sources/FerrostarCore/FerrostarCore.swift
@@ -86,6 +86,7 @@ public protocol FerrostarCoreDelegate: AnyObject {
     private var routeRequestInFlight = false
     private var lastAutomaticRecalculation: Date? = nil
     private var recalculationTask: Task<(), Never>?
+    
     private var config: NavigationControllerConfig?
 
     public init(routeAdapter: UniFFI.RouteAdapterProtocol, locationProvider: LocationProviding, networkSession: URLRequestLoading) {
