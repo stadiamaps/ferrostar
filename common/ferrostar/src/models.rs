a9b2cd10fe129b8a7ad7fbd1890916296f3d0de0
Ian Wagner <ian.wagner@stadiamaps.com>
2024-04-09 11:30:02 +0900

    Update common/ferrostar/src/models.rs

--- common/ferrostar/src/models.rs
+++ common/ferrostar/src/models.rs
@@ -253,7 +253,7 @@ pub struct SpokenInstruction {
     ///
     /// This is provided so that platform-layer integrations can easily disambiguate between distinct utterances,
     /// which may have the same textual content.
-    /// UUIds conveniently fill this purpose.
+    /// UUIDs conveniently fill this purpose.
     ///
     /// NOTE: While it is possible to deterministically create UUIDs, we do not do so at this time.
     /// This should be theoretically possible though if someone cares to write up a proposal and a PR.
