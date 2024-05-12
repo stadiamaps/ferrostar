fad3c167f0b7fb2a3945d16e17cc376dd6d91367
Ian Wagner <ian.wagner@stadiamaps.com>
2024-05-12 23:56:05 +0900

    ktfmtFormat

--- android/core/src/androidTest/java/com/stadiamaps/ferrostar/core/ValhallaCoreTest.kt
+++ android/core/src/androidTest/java/com/stadiamaps/ferrostar/core/ValhallaCoreTest.kt
@@ -297,7 +297,7 @@ class ValhallaCoreTest {
       val routes =
           core.getRoutes(
               UserLocation(
-                  GeographicCoordinate(60.5347155, -149.543469), 12.0, null,  Instant.now(), null),
+                  GeographicCoordinate(60.5347155, -149.543469), 12.0, null, Instant.now(), null),
               waypoints =
                   listOf(
                       Waypoint(
