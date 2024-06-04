e6278daad0f8d1ab6df865d0627997221efa462f
Ian Wagner <ian.wagner@stadiamaps.com>
2024-06-04 12:12:31 +0900

    Version bump to 0.1.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ if useLocalFramework {
         path: "./common/target/ios/libferrostar-rs.xcframework"
     )
 } else {
-    let releaseTag = "0.0.32"
+    let releaseTag = "0.1.0"
     let releaseChecksum = "de7220e7303bfc7c5b3629139e8e40e274d2896981437829d4bdeb1599d035df"
     binaryTarget = .binaryTarget(
         name: "FerrostarCoreRS",
