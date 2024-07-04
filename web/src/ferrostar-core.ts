bc4987c3003b5eb910d354e23779f3ff1e90757e
CatMe0w <CatMe0w@live.cn>
2024-07-03 22:19:23 -0600

    Add function stubs

--- web/src/ferrostar-core.ts
+++ web/src/ferrostar-core.ts
@@ -2,7 +2,7 @@ import { LitElement, html, css, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import leafletStyles from "leaflet/dist/leaflet.css?inline";
 import L from "leaflet";
-import init, { RouteAdapter } from "ferrostar";
+import init, { NavigationController, RouteAdapter } from "ferrostar";
 
 @customElement("ferrostar-core")
 class FerrostarCore extends LitElement {
@@ -21,6 +21,7 @@ class FerrostarCore extends LitElement {
 
   routeAdapter: RouteAdapter | null = null;
   map: L.Map | null = null;
+  navigationController: NavigationController | null = null;
 
   static styles = [
     unsafeCSS(leafletStyles),
@@ -85,6 +86,22 @@ class FerrostarCore extends LitElement {
     // FIXME: since simulated location provider is not implemented yet, we are not moving in the map!
     const polyline = L.polyline(route.geometry, { color: "red" }).addTo(this.map!);
     this.map!.fitBounds(polyline.getBounds());
+  }
+
+  async replaceRoute(route: any, config: any) {
+    // TODO
+  }
+
+  async advanceToNextStep() {
+    // TODO
+  }
+
+  async stopNavigation() {
+    // TODO
+  }
+
+  private async handleStateUpdate(newState: any, location: any) {
+    // TODO
   }
 
   render() {
