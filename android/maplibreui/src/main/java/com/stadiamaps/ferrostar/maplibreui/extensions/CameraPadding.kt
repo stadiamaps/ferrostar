2a4653459d3e2d0e384f00e84936a549d9b01e7e
Jacob Fielding <jf0517@gmail.com>
2024-07-15 20:46:43 -0700

    Adds fancy maplibre-compose-playground updates

--- android/maplibreui/src/main/java/com/stadiamaps/ferrostar/maplibreui/extensions/CameraPadding.kt
+++ android/maplibreui/src/main/java/com/stadiamaps/ferrostar/maplibreui/extensions/CameraPadding.kt
@@ -2,6 +2,10 @@ package com.stadiamaps.ferrostar.maplibreui.extensions
 
 import com.maplibre.compose.camera.models.CameraPadding
 
+/**
+ * A camera padding configuration for a navigation map view that places the user puck
+ * in the lower center of the screen.
+ */
 fun CameraPadding.Companion.NavigationPortrait(): CameraPadding {
   return CameraPadding(
       start = 0.0,
@@ -11,6 +15,10 @@ fun CameraPadding.Companion.NavigationPortrait(): CameraPadding {
   )
 }
 
+/**
+ * A camera padding configuration for a navigation map view that places the user puck
+ * in the lower end (right) of the screen.
+ */
 fun CameraPadding.Companion.NavigationLandscape(): CameraPadding {
   return CameraPadding(
       start = 1000.0,
