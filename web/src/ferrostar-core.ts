5b648d3667fbe204435adb3ff4c10e8d00865512
CatMe0w <CatMe0w@live.cn>
2024-07-25 22:26:22 -0600

    Begin the transition to MapLibre GL JS

--- web/src/ferrostar-core.ts
+++ web/src/ferrostar-core.ts
@@ -1,10 +1,7 @@
 import { LitElement, html, css, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import leafletStyles from "leaflet/dist/leaflet.css?inline";
-import L from "leaflet";
-import markerIconUrl from "../node_modules/leaflet/dist/images/marker-icon.png";
-import markerIconRetinaUrl from "../node_modules/leaflet/dist/images/marker-icon-2x.png";
-import markerShadowUrl from "../node_modules/leaflet/dist/images/marker-shadow.png";
+import maplibregl from "maplibre-gl";
+import maplibreglStyles from "maplibre-gl/dist/maplibre-gl.css?inline";
 import init, { NavigationController, RouteAdapter } from "ferrostar";
 import "./instructions-view";
 import "./arrival-view";
@@ -33,12 +30,13 @@ export class FerrostarCore extends LitElement {
   tripState: any = null;
 
   routeAdapter: RouteAdapter | null = null;
-  map: L.Map | null = null;
+  map: maplibregl.Map | null = null;
+  markers: maplibregl.Marker[] = [];
   navigationController: NavigationController | null = null;
   currentLocationMapMarker: L.Marker | null = null;
 
   static styles = [
-    unsafeCSS(leafletStyles),
+    unsafeCSS(maplibreglStyles),
     css`
       #map {
         height: 100%;
@@ -71,11 +69,6 @@ export class FerrostarCore extends LitElement {
     if (this.httpClient === fetch) {
       this.httpClient = this.httpClient.bind(window);
     }
-
-    // A workaround for loading the marker icon images in Vite
-    L.Icon.Default.prototype.options.iconUrl = markerIconUrl;
-    L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl;
-    L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
   }
 
   updated(changedProperties: any) {
@@ -85,12 +78,12 @@ export class FerrostarCore extends LitElement {
   }
 
   firstUpdated() {
-    this.map = L.map(this.shadowRoot!.getElementById("map")!).setView([0, 0], 13);
-
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(this.map);
+    this.map = new maplibregl.Map({
+      container: this.shadowRoot!.getElementById("map")!,
+      style: "https://demotiles.maplibre.org/style.json",
+      center: [0, 0],
+      zoom: 1,
+    });
   }
 
   // TODO: type
@@ -136,17 +129,44 @@ export class FerrostarCore extends LitElement {
     const initialTripState = this.navigationController.getInitialState(startingLocation);
     this.tripState = initialTripState;
 
-    this.resetMap();
+    this.clearMap();
 
-    const polyline = L.polyline(route.geometry, { color: "red" }).addTo(this.map!);
-    this.map!.fitBounds(polyline.getBounds());
+    this.map?.addSource("route", {
+      type: "geojson",
+      data: {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "LineString",
+          coordinates: route.geometry,
+        },
+      },
+    });
 
-    this.currentLocationMapMarker = L.marker(route.geometry[0]).addTo(this.map!);
+    this.map?.addLayer({
+      id: "route",
+      type: "line",
+      source: "route",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#888",
+        "line-width": 8,
+      },
+    });
+
+    this.map?.setCenter(route.geometry[0]);
+
+    const marker = new maplibregl.Marker().setLngLat(route.geometry[0]).addTo(this.map!);
+
+    this.markers.push(marker);
   }
 
   async stopNavigation() {
     // TODO: Factor out the UI layer from the core
-    this.resetMap();
+    this.clearMap();
     this.routeAdapter = null;
     this.locationProvider.updateCallback = null;
     this.navigationController = null;
@@ -158,12 +178,11 @@ export class FerrostarCore extends LitElement {
     this.currentLocationMapMarker!.setLatLng(this.locationProvider.lastLocation.coordinates);
   }
 
-  private resetMap() {
-    this.map!.eachLayer((layer) => {
-      if (layer instanceof L.Marker || layer instanceof L.Polyline) {
-        this.map!.removeLayer(layer);
-      }
-    });
+  private clearMap() {
+    this.map?.getLayer("route") && this.map?.removeLayer("route");
+    this.map?.getSource("route") && this.map?.removeSource("route");
+    this.markers.forEach((marker) => marker.remove());
+    this.markers = [];
   }
 
   render() {
