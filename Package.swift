8a9c3561352174b58c0c49d1c445d553e70ca4c7
Ian Wagner <ian.wagner@stadiamaps.com>
2024-07-31 17:44:25 +0900

    Pin MapLibre SwiftUI DSL version for now (upstream broke something)

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ if useLocalMapLibreSwiftUIDSL {
 } else {
     maplibreSwiftUIDSLPackage = .package(
         url: "https://github.com/stadiamaps/maplibre-swiftui-dsl-playground",
-        from: "0.0.18"
+        exact: "0.0.18"
     )
 }
 
