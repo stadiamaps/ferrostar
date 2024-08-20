85dd6109eee3400a82cee9b30639cc3ce0817631
Jacob Fielding <jf0517@gmail.com>
2024-08-19 17:29:12 -0700

    Added proguard rules and minor fixes

--- android/maplibreui/src/main/java/com/stadiamaps/ferrostar/maplibreui/extensions/MapViewCamera.kt
+++ android/maplibreui/src/main/java/com/stadiamaps/ferrostar/maplibreui/extensions/MapViewCamera.kt
@@ -3,11 +3,13 @@ package com.stadiamaps.ferrostar.maplibreui.extensions
 import com.maplibre.compose.camera.MapViewCamera
 
 /**
- * The centered camera configuration for navigation. This configuration sets the camera to track the
- * user, with a zoom of 16 degrees and a pitch of 45 degrees.
+ * The centered camera configuration for automotive navigation.
+ *
+ * This configuration sets the camera to track the user, with a zoom of 16 degrees and a pitch of 45
+ * degrees.
  *
  * @return The navigation MapViewCamera
  */
-fun MapViewCamera.Companion.NavigationCentered(): MapViewCamera {
+fun MapViewCamera.Companion.AutomotiveNavigationCentered(): MapViewCamera {
   return MapViewCamera.TrackingUserLocationWithBearing(zoom = 16.0, pitch = 45.0)
 }
