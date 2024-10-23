7df0fc82a828efeccf48be5bab330b5473a1bf26
Ian Wagner <ian.wagner@stadiamaps.com>
2024-10-23 17:28:37 +0900

    Rename on web

--- web/src/ferrostar-map.ts
+++ web/src/ferrostar-map.ts
@@ -13,7 +13,7 @@ import {
   TripState,
 } from "@stadiamaps/ferrostar";
 import "./instructions-view";
-import "./arrival-view";
+import "./progress-view";
 import { SimulatedLocationProvider } from "./location";
 import CloseSvg from "./assets/directions/close.svg";
 
@@ -390,7 +390,7 @@ export class FerrostarMap extends LitElement {
       <div id="map">
         <instructions-view .tripState=${this._tripState}></instructions-view>
         <div id="bottom-component">
-          <arrival-view .tripState=${this._tripState}></arrival-view>
+          <progress-view .tripState=${this._tripState}></progress-view>
           <button
             id="stop-button"
             @click=${this.stopNavigation}
