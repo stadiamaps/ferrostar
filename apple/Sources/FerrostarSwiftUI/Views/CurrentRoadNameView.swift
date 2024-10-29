711d020c24d86f8b21ed2e67e3e351aee1a6581f
Ian Wagner <ian.wagner@stadiamaps.com>
2024-10-29 17:03:03 +0900

    Extract styling of border; bump target to iOS 16 for AnyShape

--- apple/Sources/FerrostarSwiftUI/Views/CurrentRoadNameView.swift
+++ apple/Sources/FerrostarSwiftUI/Views/CurrentRoadNameView.swift
@@ -3,6 +3,9 @@ import SwiftUI
 public struct CurrentRoadNameView: View {
     let currentRoadName: String?
     let theme: any RoadNameViewTheme
+    var padding: EdgeInsets = EdgeInsets(top: 12, leading: 12, bottom: 12, trailing: 12)
+    var shape: AnyShape = AnyShape(RoundedRectangle(cornerRadius: 48))
+    var borderWidth: CGFloat = 1
 
     public init(currentRoadName: String?,
                 theme: any RoadNameViewTheme = DefaultRoadNameViewTheme())
@@ -16,13 +19,34 @@ public struct CurrentRoadNameView: View {
             Text(currentRoadName)
                 .font(theme.textFont)
                 .foregroundStyle(theme.textColor)
-                .padding(.leading, 12)
-                .padding(.trailing, 12)
-                .padding(.vertical, 8)
+                .padding(padding)
                 .background(theme.backgroundColor)
-                .clipShape(.rect(cornerRadius: 48))
-                .overlay(RoundedRectangle(cornerRadius: 48).stroke(theme.borderColor, lineWidth: 1))
+                .clipShape(shape)
+                .overlay(shape.stroke(theme.borderColor, lineWidth: borderWidth))
         }
+    }
+}
+
+public extension CurrentRoadNameView {
+    /// Sets the interior padding (expanding the buffer around the text).
+    func padding(_ padding: EdgeInsets) -> Self {
+        var newSelf = self
+        newSelf.padding = padding
+        return newSelf
+    }
+
+    /// Sets the shape for clipping and border stroke.
+    func shape<T: Shape>(_ shape: T) -> Self {
+        var newSelf = self
+        newSelf.shape = AnyShape(shape)
+        return newSelf
+    }
+
+    /// Sets the width of the border stroke.
+    func borderWidth(_ width: CGFloat) -> Self {
+        var newSelf = self
+        newSelf.borderWidth = borderWidth
+        return newSelf
     }
 }
 
