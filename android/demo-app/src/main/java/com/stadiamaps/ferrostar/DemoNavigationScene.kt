5c03f41bcbe857559ab72c709489be017652e014
ianthetechie <ianthetechie@users.noreply.github.com>
2024-11-11 01:37:47 +0000

    Apply automatic changes

--- android/demo-app/src/main/java/com/stadiamaps/ferrostar/DemoNavigationScene.kt
+++ android/demo-app/src/main/java/com/stadiamaps/ferrostar/DemoNavigationScene.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
