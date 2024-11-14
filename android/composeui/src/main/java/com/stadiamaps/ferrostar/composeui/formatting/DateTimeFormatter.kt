8a989e73ef391901819482632a23de2b84474d27
Archdoog <Archdoog@users.noreply.github.com>
2024-11-14 02:34:23 +0000

    Apply automatic changes

--- android/composeui/src/main/java/com/stadiamaps/ferrostar/composeui/formatting/DateTimeFormatter.kt
+++ android/composeui/src/main/java/com/stadiamaps/ferrostar/composeui/formatting/DateTimeFormatter.kt
@@ -1,10 +1,10 @@
 package com.stadiamaps.ferrostar.composeui.formatting
 
 import android.icu.util.ULocale
+import java.time.format.FormatStyle
 import java.util.Locale
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.toJavaLocalDateTime
-import java.time.format.FormatStyle
 
 interface DateTimeFormatter {
   fun format(dateTime: LocalDateTime): String
@@ -16,9 +16,7 @@ class EstimatedArrivalDateTimeFormatter(
 ) : DateTimeFormatter {
   override fun format(dateTime: LocalDateTime): String {
     val locale = locale.let { Locale(it.language, it.country) }
-    val formatter =
-        java.time.format.DateTimeFormatter.ofLocalizedTime(unitStyle)
-            .withLocale(locale)
+    val formatter = java.time.format.DateTimeFormatter.ofLocalizedTime(unitStyle).withLocale(locale)
     return formatter.format(dateTime.toJavaLocalDateTime())
   }
 }
