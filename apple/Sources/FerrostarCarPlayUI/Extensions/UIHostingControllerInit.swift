af638ce915365e852df718ab73d05f858fbb6395
Jacob Fielding <jf0517@gmail.com>
2024-11-20 19:00:48 -0800

    feat: basic CarPlay init

--- apple/Sources/FerrostarCarPlayUI/Extensions/UIHostingControllerInit.swift
+++ apple/Sources/FerrostarCarPlayUI/Extensions/UIHostingControllerInit.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 import UIKit
 
+// swiftformat:disable all
 extension UIHostingController where Content: View {
     /// Create a SwiftUI view in a UIHosting controller with a closure init.
     ///
     /// - Parameter content: The convent view builder.
-    convenience init(@ViewBuilder content: () -> some View) {
+    convenience init<V: View>(@ViewBuilder content: () -> V) where V == Content {
         self.init(rootView: content())
     }
 }
+// swiftformat:enable all
