4e03eaa00de360fe178161a0b2ee9b752f33bc55
Benjamin <btrounson@outlook.co.nz>
2024-12-10 16:08:14 +1300

    Initial Compilable Example

--- expo/example/App.tsx
+++ expo/example/App.tsx
@@ -1,19 +1,31 @@
 import { Ferrostar, FerrostarProvider } from "expo-ferrostar";
-import { SafeAreaView } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <FerrostarProvider>
-        <Ferrostar style={{ flex: 1, width: "100%" }} />
-      </FerrostarProvider>
-    </SafeAreaView>
+            <Ferrostar
+                style={{ flex: 1 }}
+                locationMode="default"
+                styleUrl="https://api.maptiler.com/maps/basic-v2/style.json?key=DQTRKy0N0WUe90KUjN4i"
+                snapUserLocationToRoute={false}
+                navigationControllerConfig={{
+                    stepAdvance: {
+                        minimumHorizontalAccuracy: 25,
+                        automaticAdvanceDistance: 10,
+                    },
+                    routeDeviationTracking: {
+                        minimumHorizontalAccuracy: 15,
+                        maxAcceptableDeviation: 50.0
+                    },
+                    courseFiltering: "SNAP_TO_ROUTE"
+                }}
+            />
   );
 }
 
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: "#eee",
+    backgroundColor: "#FFF",
   },
 };
