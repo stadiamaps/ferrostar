ae1feff0908fc86c1a31a07406e7876f5b1858a6
Benjamin Trounson <btrounson@outlook.co.nz>
2024-12-21 19:24:37 +1300

    Location Provider Implementation

---
+++ react-native/src/core/LocationProvider.ts
@@ -0,0 +1,82 @@
+import Geolocation, {
+  type GeolocationConfiguration,
+} from '@react-native-community/geolocation';
+import {
+  GeographicCoordinate,
+  UserLocation,
+  type Heading,
+} from '../generated/ferrostar';
+
+export interface LocationProviderInterface {
+  lastLocation?: UserLocation;
+  lastHeading?: Heading;
+  addListener(listener: LocationUpdateListener): void;
+  removeListener(listener: LocationUpdateListener): void;
+}
+
+export interface LocationUpdateListener {
+  onLocationUpdate(location: UserLocation): void;
+  onnHeadingUpdate(heading: Heading): void;
+}
+
+export class LocationProvider implements LocationProviderInterface {
+  lastLocation?: UserLocation;
+  lastHeading?: Heading;
+
+  private listeners: Map<LocationUpdateListener, number> = new Map();
+
+  constructor(config: GeolocationConfiguration) {
+    Geolocation.setRNConfiguration(config);
+  }
+
+  /**
+   * Adds a location update listener.
+   *
+   * NOTE: This does NOT attempt to check permissions. The caller is responsible for ensuring that
+   * permissions are enabled before calling this.
+   */
+  addListener(listener: LocationUpdateListener): void {
+    console.log('LocationProvider', 'Add location listener');
+    if (this.listeners.has(listener)) {
+      console.log('LocationProvider', 'Already registered; skipping');
+      return;
+    }
+
+    const watchId = Geolocation.watchPosition((location) => {
+      const { coords, timestamp } = location;
+      const userLocation = UserLocation.new({
+        coordinates: GeographicCoordinate.new({
+          lat: coords.latitude,
+          lng: coords.longitude,
+        }),
+        horizontalAccuracy: coords.accuracy,
+        // TODO: map these parameters to the correct types not 100% which ones are correct
+        courseOverGround: undefined,
+        speed:
+          coords.speed !== null
+            ? {
+                value: coords.speed,
+                accuracy: undefined,
+              }
+            : undefined,
+        timestamp: new Date(timestamp),
+      });
+      listener.onLocationUpdate(userLocation);
+    });
+
+    this.listeners.set(listener, watchId);
+  }
+
+  removeListener(listener: LocationUpdateListener): void {
+    console.log('LocationProvider', 'Remove location listener');
+    const watchId = this.listeners.get(listener);
+
+    if (watchId === undefined) {
+      return;
+    }
+
+    Geolocation.clearWatch(watchId);
+  }
+}
+
+// TODO: Add simulated provider
