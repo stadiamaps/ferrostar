3417e4a0270d6f8afbc99ff1be1548edff26ba9d
Benjamin Trounson <btrounson@outlook.co.nz>
2025-01-16 22:15:22 +1300

    feat: add example app with dotenv

--- react-native/example/babel.config.js
+++ react-native/example/babel.config.js
@@ -7,6 +7,7 @@ const root = path.resolve(__dirname, '..');
 module.exports = getConfig(
   {
     presets: ['module:@react-native/babel-preset'],
+    plugins: ['module:react-native-dotenv']
   },
   { root, pkg }
 );
--- react-native/example/src/App.tsx
+++ react-native/example/src/App.tsx
@@ -1,12 +1,124 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-ferrostar';
+import { Button, StyleSheet, View } from 'react-native';
+import {
+  CourseFiltering,
+  FerrostarCore,
+  NavigationView,
+  RouteDeviationTracking,
+  SpecialAdvanceConditions,
+  StepAdvanceMode,
+  WaypointKind,
+} from 'react-native-ferrostar';
+import Geolocation, {
+  type GeolocationResponse,
+} from '@react-native-community/geolocation';
+import { useEffect, useMemo, useState } from 'react';
 
-const result = multiply(3, 7);
+const apiKey = process.env.STADIA_MAPS_API_KEY ?? "";
+const styleUrl = `https://tiles.stadiamaps.com/styles/alidade_smooth.json?api_key=${apiKey}`;
 
 export default function App() {
+  const [location, setLocation] = useState<GeolocationResponse | null>(null);
+
+  const core = useMemo(
+    () =>
+      new FerrostarCore('https://valhalla1.openstreetmap.de/route', 'auto', {
+        stepAdvance: StepAdvanceMode.RelativeLineStringDistance.new({
+          minimumHorizontalAccuracy: 25,
+          specialAdvanceConditions:
+            SpecialAdvanceConditions.MinimumDistanceFromCurrentStepLine.new(10),
+        }),
+        routeDeviationTracking: RouteDeviationTracking.StaticThreshold.new({
+          minimumHorizontalAccuracy: 15,
+          maxAcceptableDeviation: 50,
+        }),
+        snappedLocationCourseFiltering: CourseFiltering.SnapToRoute,
+      }),
+    []
+  );
+
+  useEffect(() => {
+    Geolocation.requestAuthorization();
+
+    if (location === null) {
+      Geolocation.getCurrentPosition(
+        (l) => {
+          setLocation(l);
+        },
+        (e) => {
+          console.log(e);
+        },
+        {
+          enableHighAccuracy: true,
+          fastestInterval: 0,
+          interval: 1000,
+        }
+      );
+    }
+
+    const watchId = Geolocation.watchPosition(
+      (l) => {
+        setLocation(l);
+      },
+      (e) => {
+        console.log(e);
+      },
+      {
+        enableHighAccuracy: true,
+        fastestInterval: 0,
+        interval: 1000,
+      }
+    );
+
+    return () => {
+      Geolocation.clearWatch(watchId);
+    };
+  }, [location]);
+
+  const handleNavigationStart = async () => {
+    if (!location) {
+      return;
+    }
+
+    const { coords, timestamp } = location;
+    const routes = await core.getRoutes(
+      {
+        coordinates: { lat: coords.latitude, lng: coords.longitude },
+        horizontalAccuracy: coords.accuracy,
+        speed: undefined,
+        courseOverGround: undefined,
+        timestamp: new Date(timestamp),
+      },
+      [
+        {
+          coordinate: { lat: coords.latitude, lng: coords.longitude },
+          kind: WaypointKind.Break,
+        },
+        {
+          coordinate: {
+            lat: -43.56823546768915,
+            lng: 172.6902914460014,
+          },
+          kind: WaypointKind.Break,
+        },
+      ]
+    );
+
+    const route = routes[0];
+    if (!route) {
+      return;
+    }
+
+    core.startNavigation(route);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <NavigationView
+        style={styles.container}
+        mapStyle={styleUrl}
+        core={core}
+      />
+      <Button title="Start Navigation" onPress={handleNavigationStart} />
     </View>
   );
 }
@@ -14,7 +126,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
