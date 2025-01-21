fcd3a89a7c285bed07cac4d4fe131dc192323ecb
Benjamin Trounson <benjamint@contecgroup.com>
2025-01-21 16:46:07 +1300

    chore: delete auto-generated code

--- react-native/src/views/index.tsx
+++
@@ -1,5 +0,0 @@
-import NavigationView from './NavigationView';
-import NavigationMapViewCamera from './NavigationMapViewCamera';
-import BorderedPolyline from './BorderedPolyline';
-
-export { NavigationView, NavigationMapViewCamera, BorderedPolyline };
