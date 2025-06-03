1aa482d5ca36c513329bfd1a8c0b135e9ecb05d7
Ben Burwood <ben.burwood@streetwave.co>
2025-06-03 15:42:31 +0100

    Update Moshi Usage to Kotlinx Serialziation

--- android/core/src/main/java/com/stadiamaps/ferrostar/core/annotation/valhalla/ValhallaOSRMExtendedAnnotation.kt
+++ android/core/src/main/java/com/stadiamaps/ferrostar/core/annotation/valhalla/ValhallaOSRMExtendedAnnotation.kt
@@ -1,11 +1,14 @@
 package com.stadiamaps.ferrostar.core.annotation.valhalla
 
-import com.squareup.moshi.Json
 import com.stadiamaps.ferrostar.core.annotation.Speed
+import com.stadiamaps.ferrostar.core.annotation.SpeedSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ValhallaOSRMExtendedAnnotation(
     /** The speed limit of the segment. */
-    @Json(name = "maxspeed") val speedLimit: Speed?,
+    @SerialName("maxspeed") @Serializable(with = SpeedSerializer::class) val speedLimit: Speed?,
     /** The estimated speed of travel for the segment, in meters per second. */
     val speed: Double?,
     /** The distance in meters of the segment. */
