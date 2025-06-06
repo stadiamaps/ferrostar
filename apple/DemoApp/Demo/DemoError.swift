f1911ba7d86d9339d0326d10b7b18c2016686f5f
Greg Bolsinga <bolsinga@mac.com>
2025-06-06 08:07:41 -0700

    iOS: Add Observable DemoModel
    - Naming things is hard: This adds `DemoNavigationState`. It is the big-picture states of a navigation appliation. It iterates throught .idle, .chooseDestination, load .routes, .selectRoute, .startNavigation
    - The Demo app doesn't have a way (right now) to pick a destination, nor choose from multiple routes. And the plan it loads only ever returns one route. However a Navigation App and library needs to support all of these states. So right now this adds a button to move the UI through these states. This will be *very useful* for having this state in CarPlay.
    - This is odd UI, but the the improvements to the DemoApp have to start somehow.
    - This also separates the parts out into separate files.
    - It removes the need for `AppEnvironment`, and `@EnvironmentObject` completely.
    - The minimum OS version for the DemoApp (but not the Ferrostar library) is iOS17, so this uses `@Observable` for the `DemoModel`.
    - Since `FerrorstarCore` must remain `ObservableObject` and use Combine, use Combine to subscribe to the Published properties and then just have it set Observable properties in the `DemoModel`.
    
    - This depends upon unlanded PRs. However I'm putting this up now.

---
+++ apple/DemoApp/Demo/DemoError.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum DemoError: Error {
+    case invalidOrigin
+    case invalidDestination
+    case noRoutesLoaded
+    case noFirstRoute
+    case invalidState(DemoNavigationState)
+    case invalidCPRouteChoice
+}
