155dec18031689bc87e53c061a469bd298b469c5
Jacob Fielding <jf0517@gmail.com>
2025-07-28 18:25:51 -0700

    feat: dynamic island

--- apple/Sources/FerrostarCore/WidgetProviding.swift
+++ apple/Sources/FerrostarCore/WidgetProviding.swift
@@ -1,6 +1,16 @@
 import FerrostarCoreFFI
 
+/// The widget provided is given to ``FerrostarCore/FerrostarCore`` and is called when the navigation state is updated
+/// by a user
+/// update or navitation is stopped.
 public protocol WidgetProviding {
+    /// Update _or create_ the widget with a new status
+    ///
+    /// - Parameters:
+    ///   - visualInstruction: The latest visual instruction provided by the navigation state.
+    ///   - tripProgress: The latest trip progress provided by the navigation state.
     func update(visualInstruction: VisualInstruction, tripProgress: TripProgress)
+
+    /// Terminate the session.
     func terminate()
 }
