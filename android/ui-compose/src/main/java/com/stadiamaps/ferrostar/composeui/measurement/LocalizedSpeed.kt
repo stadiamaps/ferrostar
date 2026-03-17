12dc5e89ea1630d17d8d1dd1b4c1a8170a1d669a
Jacob Fielding <jf0517@gmail.com>
2026-03-16 17:45:04 -0700

    removes unused files missed in module rename

--- android/ui-compose/src/main/java/com/stadiamaps/ferrostar/composeui/measurement/LocalizedSpeed.kt
+++
@@ -1,6 +0,0 @@
-package com.stadiamaps.ferrostar.composeui.measurement
-
-import android.content.Context
-import com.stadiamaps.ferrostar.composeui.R
-import com.stadiamaps.ferrostar.core.measurement.MeasurementSpeedUnit
-
