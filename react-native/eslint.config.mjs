f50360a6758daed7837ef7b0cbf26384b6f9c816
Ben Trounson <btrounson@outlook.co.nz>
2026-09-15 15:04:48 +1200

    eslint config updates

--- react-native/eslint.config.mjs
+++ react-native/eslint.config.mjs
@@ -1,11 +1,14 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import reactYouMightNotNeedAnEffect from 'eslint-plugin-react-you-might-not-need-an-effect';
 import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+export default defineConfig([
+  { ignores: ['**/lib/**', '**/*.config.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{ts,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-];
+  reactYouMightNotNeedAnEffect.configs.recommended,
+]);
